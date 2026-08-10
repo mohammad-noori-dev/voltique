9d870e650c3c09024ff0ae50c0c9f5ee2dd5edab
MNoori-coder <72785416+code-lover-forever@users.noreply.github.com>
2026-08-10 14:30:44 +0330

    wsgi.py is added to root directory.

---
+++ wsgi.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, BASE_DIR)
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voltique.settings")
+
+from django.core.wsgi import get_wsgi_application
+
+application = get_wsgi_application()
